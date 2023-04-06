59c0d3b9a2b74a8c48298ef7fe0a8f0ba33cac30
malcom <rainmalcom@gmail.com>
2023-04-06 16:04:29 -0300

    spring railway app config

---
+++ src/test/java/com/company/springdeploy/SpringDeployApplicationTests.java
@@ -0,0 +1,16 @@
+package com.company.springdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeployApplicationTests {
+
+	@Test     // CON ESTO SE PUEDE IMPRIMIR LAS VARIABLES DE ENTORNO
+	void contextLoads() {
+			System.getenv().forEach(
+					(key, value) -> System.out.println(key + " " + value)
+			);
+	}
+
+}
